6d9c4360a98935cf3d5377f9e1aff4eaa987d84b
Phumlani Arendse <aphumlani.dev@gmail.com>
2020-09-13 18:14:55 +0200

    first commit

---
+++ ReverseText/ReverseText/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace ReverseText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //read user input
+            string name = Console.ReadLine();
+
+            //store user input to char array
+            char[] chars = name.ToCharArray();
+
+            //used result char array to store output
+            char[] result = new char[chars.Length];
+
+            //here we swap the first value with last value until the whole array is reversed
+            for (int i = 0, j = name.Length - 1; i < name.Length; i++, j--)
+            {
+                result[i] = chars[j];
+               }
+
+                Console.WriteLine(result.ToArray());
+        }          
+    }
+}
